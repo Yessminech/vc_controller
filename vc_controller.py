a22fa1583ef9903360b1f9e64251022aa94b57e1
Yessmine <chabchoub.yessmine@gmail.com>
2024-06-28 15:05:49 +0200

    add working py script

--- vc_controller.py
+++ vc_controller.py
@@ -1,36 +1,38 @@
 import Jetson.GPIO as GPIO
 import time
 
-def configure_pin(remote_on_off_pin=9):
+PIN_NUM = 7
+# Update the pin number according to your hardware setup
+def configure_pin(remote_on_off_pin=PIN_NUM): 
     """
     Configures the GPIO pin for remote on/off control.
 
     Args:
-        remote_on_off_pin (int): Pin number for remote on/off control (default is 9).
+        remote_on_off_pin (int): Pin number for remote on/off control.
     """
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(remote_on_off_pin, GPIO.OUT)
 
-def enable_converter(remote_on_off_pin=9):
+def enable_converter(remote_on_off_pin=PIN_NUM):
     """
     Enables the converter using the specified GPIO pin.
 
     Args:
-        remote_on_off_pin (int): Pin number for remote on/off control (default is 9).
+        remote_on_off_pin (int): Pin number for remote on/off control.
     """
     GPIO.output(remote_on_off_pin, GPIO.HIGH)
 
-def disable_converter(remote_on_off_pin=9):
+def disable_converter(remote_on_off_pin=PIN_NUM):
     """
     Disables the converter and performs GPIO cleanup.
 
     Args:
-        remote_on_off_pin (int): Pin number for remote on/off control (default is 9).
+        remote_on_off_pin (int): Pin number for remote on/off control.
     """
     GPIO.output(remote_on_off_pin, GPIO.LOW)
     GPIO.cleanup()
 
-def reboot_converter(sleep_time_seconds: int = 20, sleep_time_minutes: float = 20/60) -> None:
+def reboot_converter(sleep_time_seconds: int = 5, sleep_time_minutes: float = 5/60) -> None:
     """
     Reboots the converter by enabling and disabling it after a specified sleep time.
 
@@ -43,3 +45,14 @@ def reboot_converter(sleep_time_seconds: int = 20, sleep_time_minutes: float = 2
     enable_converter()
     time.sleep(cycle_time_s)
     disable_converter()
+
+if __name__ == "__main__":
+    configure_pin()
+    reboot_converter()
+
+#jnx45@jnx45-desktop:/sys/class/gpio$ sudo chmod 777 unexport
+#jnx45@jnx45-desktop:/sys/class/gpio$ sudo chmod 777 export
+#328 GPIO04 [https://developer.download.nvidia.com/assets/embedded/secure/jetson/orin_nx/docs/Jetson_Orin_NX_DS-10712-001_v0.5.pdf?6zfO8L1bTgeh6Kw_6S3fWXdLZDBSWonJZFpHgSUIIOuUfXTDRLsCpoLmebc3bqCBK2Pp4R9pGo4VVyw9-pwsySBqm4hnE0ZiyXORj9LYUDPeMI5xTXmO28pE50RJOaSXM9lg8AoOS7hIyKqP2jHajumEFOWR8TRQtbhdOzmFShvcO8N0jdIRRO-yCXrdNg==&t=eyJscyI6ImdzZW8iLCJsc2QiOiJodHRwczovL3d3dy5nb29nbGUuY29tLyJ9]
+#GPIOnumber = GPIOletter*8 + GPIOnumber + GPIOoffset
+# Jetson Nano -> GPIOoffset = 0 [https://auvidea.eu/download/manual/JNX42/JNX42_Manual.pdf]
+# 340 -> EE01
---
+++ vs_controller_auvidea.py
@@ -0,0 +1,42 @@
+import os
+import time
+
+def export_gpio(pin):
+    with open("/sys/class/gpio/export", 'w') as f:
+        f.write(str(pin))
+
+def unexport_gpio(pin):
+    with open("/sys/class/gpio/unexport", 'w') as f:
+        f.write(str(pin))
+        
+def set_gpio_direction(pin, direction):
+    with open(f"/sys/class/gpio/"+ gpiodir + "/direction", 'w') as f:
+        f.write(direction)
+
+def set_gpio_value(pin, value):
+    with open(f"/sys/class/gpio/"+ gpiodir + "/value", 'w') as f:
+        f.write(str(value))
+
+def read_gpio_value(pin):
+    with open(f"/sys/class/gpio/"+ gpiodir + "/value", 'r') as f:
+        return f.read().strip()
+
+def perform_reboot(pin, delay):
+    # Check if the pin is already exported
+    if os.path.exists(f"/sys/class/gpio/"+ gpiodir ):
+        # print("Pin is already exported. Unexporting...")
+        unexport_gpio(pin)
+    export_gpio(pin)
+    set_gpio_direction(pin, "out")
+    print("Controller is now rebooting. Please wait for the system to finish rest")
+    set_gpio_value(pin, 0) 
+    time.sleep(delay)
+    set_gpio_value(pin, 1)
+    unexport_gpio(pin) 
+    print("Reboot performed successfully!")
+    
+# Example usage for pin 329
+pin = 329
+delay = 1
+gpiodir= "PCC.01"
+perform_reboot(pin, delay)
